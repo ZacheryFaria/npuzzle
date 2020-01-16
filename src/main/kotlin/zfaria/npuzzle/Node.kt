4d4a277e0de24dabf64588a6a2032a34c484bd4b
Zachery Faria <zacheryfaria@gmail.com>
2020-01-15 19:53:23 -0800

    Push for end of day

--- src/main/kotlin/zfaria/npuzzle/Node.kt
+++ src/main/kotlin/zfaria/npuzzle/Node.kt
@@ -7,9 +7,23 @@ class Node(val size: Int, var value: List<Int>) {
 
     var hashFunction = Hashing.farmHashFingerprint64()
 
+    val zeroIndex = value.indexOf(0)
+
+    private fun getX(n: Int): Int {
+        return n % size
+    }
+
+    private fun getY(n: Int): Int {
+        return n / size
+    }
+
+    fun isValid(): Boolean {
+        val set = setOf(value)
+        return set.size == value.size
+    }
+
+
     override fun hashCode(): Int {
         return hashFunction.hashString(value.toString(), Charset.defaultCharset()).asInt()
     }
 }
-
-//142604
